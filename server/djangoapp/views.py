5c68fb49d3c7d9bd2af2a8d930843746a1c52085
abood <abood.alsh2021@gmail.com>
2024-08-12 08:01:55 -0400

    mid module 4

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -111,9 +111,9 @@ def get_dealer_reviews(request, dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review_details in reviews:
-            response = analyze_review_sentiments(review_detail['review'])
+            response = analyze_review_sentiments(review_details['review'])
             print(response)
-            review_detail['sentiment'] = response['sentiment']
+            review_details['sentiment'] = response['sentiment']
         return JsonResponse({"status":200,"reviews":reviews})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
