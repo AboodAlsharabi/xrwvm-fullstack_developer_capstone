3cfcf9ea0e03d53aa9081c0dc5d24df5559711c9
Dell <abood.alsh2021@gmail.com>
2024-08-14 23:41:39 +0300

    final v8

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -6,8 +6,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    
+    description = models.TextField() 
     def __str__(self):
         return self.name
 
@@ -26,6 +25,5 @@ class CarModel(models.Model):
         MaxValueValidator(2023),
         MinValueValidator(2015)
         ])
-    
     def __str__(self):
         return self.name  # Return the name as the string representation
